c496081a14a6778717f5c0a5d044c2171b7e9fde
SlingShot121 <lightningnite843@gmail.com>
2023-05-18 15:19:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // script.js
-function handleClick() {
-    // Add functionality here
+function fetchExchangeRate() {
+    const apiKey = 'YOUR_API_KEY';
+    const url = `https://v6.exchangeratesapi.io/latest?base=USD&symbols=ZAR&apiKey=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.rates && data.rates.ZAR) {
+                const exchangeRate = data.rates.ZAR;
+                document.getElementById('exchangeRate').innerText = `1 USD = ${exchangeRate} ZAR`;
+            } else {
+                document.getElementById('exchangeRate').innerText = 'Unable to fetch exchange rate';
+            }
+        })
+        .catch(error => {
+            console.log('Error fetching exchange rate:', error);
+            document.getElementById('exchangeRate').innerText = 'Error fetching exchange rate';
+        });
 }
