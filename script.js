8dfb935b92d26244742071d468acebdb870836b6
SlingShot121 <lightningnite843@gmail.com>
2023-05-18 15:07:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const apiKey = '78197c7f477d248dc54db6ad';
-const url = `https://v6.exchangeratesapi.io/latest?base=USD&symbols=ZAR&apiKey=${apiKey}`;
+function loadExchangeRates() {
+    const apiKey = '78197c7f477d248dc54db6ad';
+    const url = `https://v6.exchangeratesapi.io/latest?base=USD&symbols=ZAR&apiKey=${apiKey}`;
 
-fetch(url)
-   .then(response => response.json())
-   .then(data => {
-       if (data && data.rates && data.rates.ZAR) {
-           const exchangeRate = data.rates.ZAR;
-           document.getElementById('exchangeRate').innerText = `1 USD = ${exchangeRate} ZAR`;
-       } else {
-           document.getElementById('exchangeRate').innerText = 'Unable to fetch exchange rate';
-       }
-   })
-   .catch(error => {
-       console.log('Error fetching exchange rate:', error);
-       document.getElementById('exchangeRate').innerText = 'Error fetching exchange rate';
-   });
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.rates && data.rates.ZAR) {
+                const exchangeRate = data.rates.ZAR;
+                document.getElementById('exchangeRate').innerText = `1 USD = ${exchangeRate} ZAR`;
+            } else {
+                document.getElementById('exchangeRate').innerText = 'Unable to fetch exchange rate';
+            }
+        })
+        .catch(error => {
+            console.log('Error fetching exchange rate:', error);
+            document.getElementById('exchangeRate').innerText = 'Error fetching exchange rate';
+        });
+}
